4f304e2900bf24b3ca4bd70baac8882516957c14
pandeyprashant123-coder <83771967+pandeyprashant123-coder@users.noreply.github.com>
2023-01-03 22:45:56 +0545

    haha

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Setup from './tutorial/6-useReducer/setup'
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup/>
     </div>
   )
 }
